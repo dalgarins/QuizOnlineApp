4879cf9d0c12707d84add393f3c11bc2ba97762e
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2018-07-29 13:38:04 -0500

    adding domain layers

--- domain/src/main/java/co/anbora/onlinequizapp/domain/model/Question.kt
+++ domain/src/main/java/co/anbora/onlinequizapp/domain/model/Question.kt
@@ -1,2 +1,8 @@
 package co.anbora.onlinequizapp.domain.model
 
+data class Question(val question: String = "",
+                    val answerA: String = "",
+                    val answerB: String = "",
+                    val answerC: String = "",
+                    val answerD: String = "",
+                    val categoryId: String = "", val isImageQuestion: String = "")
